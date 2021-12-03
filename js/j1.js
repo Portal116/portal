b86b4803f6edbcff628e79f2a679225d4c679735
Portal116 <tim950116@gmail.com>
2021-12-03 12:14:25 +0900

    20211203 1214

---
+++ js/j1.js
@@ -0,0 +1,42 @@
+// 자바 스크립트 코드 작성
+
+// alert('안녕하세요');
+
+// document.body.innerHTML = "<h1>안녕하세요!</h1>"
+
+// var r = 'R' + 'a' + 'p';
+// var r2 = 10+5+20;
+// alert(r+r2);
+
+// alert(" he"); 함수
+// Array.length; Array 클래스의 변수 = 속성
+// InputEvent; 변수
+// prompt('Message', 'AAA'); 함수
+// Math.PI; 속성
+// Math.abs(-108); 메소드
+
+// let tel = '5555';
+// alert("동해물과 \t 백두산이 \n 마르고 It's ");
+
+// let name = '홍길동 김유신 최민영';
+// console.log(name.length);
+// console.log(name[0] + '씨');
+// console.log(name[4] + '씨');
+// console.log(name[8] + '씨');
+
+let num1 = 10;
+let num2 = 20;
+if(num1>num2)
+    console.log(num1);
+else console.log(num2);
+console.log(typeof(num1));
+console.log(typeof(' `이름` '));
+console.log(typeof(true));
+console.log(typeof(` ${num1} === ${num2} `));
+console.log(` ${num1} === ${num2} `);
+console.log(1 == "1");
+console.log(1 === "1");
+console.log( false == "0"); // false는 0이므로 true나옴
+console.log( false === "0");
+console.log("" == []); // ""는 false 반환, []도 false반환
+console.log(0 == []);
---
+++ js/j2.js
@@ -0,0 +1,48 @@
+// const pi = 3.14;
+// pi = 4.5;
+// console.log(pi);
+// console.log(name);
+
+
+// let pAge = 10;
+// pAge -= 5;
+// console.log(pAge);
+
+// pAge = 30;
+// pAge *= 10;
+// console.log(++pAge);
+
+
+// let list='';
+
+// list += '<ul>';
+// list += '   <li>사과</li>';
+// list += '   <li>딸기</li>';
+// list += '   <li>바나나</li>';
+// list += '   <li>포도</li>';
+// list += '</ul>';
+// document.write(list);
+
+
+// let number = 10;
+// document.write(number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+// document.write(++number + "&nbsp;");
+
+
+// let sNum = "123";
+// console.log(sNum);
+
+// intNum = Number(sNum);
+// console.log(intNum);
+
+
+const input = prompt('숫자입력','기본값');
+alert(Number(input) + 100);
