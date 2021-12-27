17b697e8ee9ffe129db16ae037b2a62ecbbd0338
Portal116 <tim950116@gmail.com>
2021-12-27 11:21:15 +0900

    20211227 1121

--- shoppingMall/src/Test.java
+++ shoppingMall/src/Test.java
@@ -1,15 +1,9 @@
-
 public class Test {
 	public static void main(String[] args) {
-		
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		if(obj1.hashCode(obj2)) {
+			System.out.println("1");
+		}
 	}
-public static void print(Employee e) {
-	if(e instanceof Salesman) {
-		System.out.println("Salesman입니다.");
-	} else if(e instanceof Manager) {
-		System.out.println("Manager입니다.");
-	} if(e instanceof Consultant) {
-		System.out.println("Consultant입니다.");
-	} 
-}
 }
